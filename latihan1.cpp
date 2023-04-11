ccc562a3e0fd9ffb929705db27c1e3950ae3ac62
MuhammadAHM <88298105+saino36@users.noreply.github.com>
2023-04-11 13:48:31 +0700

    Add files via upload

---
+++ latihan1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << "Masukkan nilai: " << endl;
+    cin >> i;
+    cout << "Nilai yang dimasukkan " << i << endl;
+    cout << "Jika nilai digandakan " << i * 2 << endl;
+    return 0;
+}
